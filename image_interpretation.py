b6d8cfce1c700f00d346a3920af15211396333e9
ceciliabjensen <ceciliabjensen@gmail.com>
2020-10-30 10:21:17 +0100

    Update image_interpretation.py

--- image_interpretation.py
+++ image_interpretation.py
@@ -5,12 +5,9 @@ Created on Thu Oct 15 12:52:55 2020
 @author: Cecilia Bang Jensen
 """
 
-import data_check as dc
+import FlucAnalysis.data_functions as data_tools
 import pandas as pd
 import numpy as np
-
-test = pd.DataFrame(np.array([[0,30,60,90,120],[4,6,9,5,3]]))
-
 
 
 def image_interpretation(postprandial_data, num_interp_pts=100, grid_size=10, interpolation_type='linear', lower_bound=None, upper_bound=None):
@@ -22,23 +19,14 @@ def image_interpretation(postprandial_data, num_interp_pts=100, grid_size=10, in
 
 	
 	# Class object is created
-	data_info = dc.dataClass(postprandial_data, num_interp_pts, grid_size, interpolation_type, lower_bound, upper_bound)
+	data_info = data_tools.dataClass(postprandial_data, num_interp_pts, grid_size, interpolation_type, lower_bound, upper_bound)
 	
 	# Data check is performed 
-	dc.dataClass.data_check(data_info,nan=False)
+	data_tools.dataClass.data_check(data_info,nan=False)
 	
 	# Create image
-	dc.dataClass.image(data_info)
+	data_tools.dataClass.image(data_info)
 	
 	
 	return(data_info)
 	
-
-
-#resulting_data = image_interpretation(test,100,10,'cubic')
-resulting_data = image_interpretation(test)
-
-#print(resulting_data.num_timepoints)
-print(resulting_data.image)
-
-
