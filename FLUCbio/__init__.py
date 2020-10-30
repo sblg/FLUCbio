eb51c1f6e82c75e7cdfc9345eb0b165c224ac02c
ceciliabjensen <ceciliabjensen@gmail.com>
2020-10-30 07:52:25 +0100

    Update and rename FLUCbio/__init__.py to __init__.py

--- FLUCbio/__init__.py
+++
@@ -1,5 +0,0 @@
-from FLUCbio.functions.data_functions import dataClass
-import FLUCbio.functions.clust_sum
-import FLUCbio.functions.fluc_measure
-import FLUCbio.functions.image_interpretation
-import FLUCbio.functions.impute_data
---
+++ __init__.py
@@ -0,0 +1,9 @@
+name = "FlucAnalysis"
+__version__ = "0.0.1"
+__author__ = "Cecilia Bang Jensen"
+
+from .data_functions import dataClass
+import .clust_sum
+import .fluc_measure
+import .image_interpretation
+import .impute_data
