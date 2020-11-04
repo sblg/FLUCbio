58ddf85c16095f1a473d84a7504b881cd46e7a0c
ceciliabjensen <ceciliabjensen@gmail.com>
2020-11-04 07:37:50 +0100

    Update impute_data.py

--- impute_data.py
+++ impute_data.py
@@ -5,18 +5,6 @@ Created on Thu Oct 15 14:10:48 2020
 @author: Cecilia Bang Jensen
 """
 import FLUCbio.data_functions as data_tools
-import numpy as np  # can be removed
-import pandas as pd  # can be removed
-
-
-# Make different test objects. tuple/listoflists and not this type. negative values
-# imaginary?  missing values. only zeros. strings. dataframes. integers. 
-# too many missing values. no missing values. Not same length of 1 and 2 element
-
-
-# check if too many nans next to each other??
-# more than two holes next to each other (I think i have.. or it is in general more than 2?)
-# implement when there is both nan and a hole
 
 
 def impute_data(postprandial_data, imputation_type='linear'):
@@ -28,16 +16,10 @@ def impute_data(postprandial_data, imputation_type='linear'):
 	# Data check is performed 
 	data_tools.dataClass.data_check(data_info,nan=True)
 	
-	# Check if holes in time ()unevenly) and if so add extra imputed measurements
+	# Check if holes in time (unevenly) and if so add extra imputed measurements
 	data_tools.dataClass.insert_missing(data_info)	
 	
-	# Do imputation
+	# Do imputation and return class
 	data_tools.dataClass.impute_data(data_info)	
 	return(data_info)
-
-
-#resulting_data = impute_data(test,imputation_type='cubic')
-
-# linear can not run if first element is nan?
-# ValueError: A value in x_new is below the interpolation range.
 
