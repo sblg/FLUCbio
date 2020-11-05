2788797175e1870564d26b868a8d66af770fce15
ceciliabjensen <ceciliabjensen@gmail.com>
2020-11-05 09:45:09 +0100

    Update impute_data.py

--- impute_data.py
+++ impute_data.py
@@ -7,19 +7,19 @@ Created on Thu Oct 15 14:10:48 2020
 import FLUCbio.data_functions as data_tools
 
 
-def impute_data(postprandial_data, imputation_type='linear', adj_nan=2):
+def impute_data(postprandial_data, imputation_type='linear', adj_nan=2, nan=True):
 	""" Imputes missing data or add imputed measures for unevenly distributed data """
 	
 	# Class object is created
 	data_info = data_tools.dataClass(postprandial_data, imputation_type=imputation_type)
 	
 	# Data check is performed 
-	data_tools.dataClass.data_check(data_info, nan=True)
+	data_info.data_check(nan=nan)
 	
 	# Check if holes in time (unevenly) and if so add extra imputed measurements
-	data_tools.dataClass.insert_missing(data_info)	
+	data_info.insert_missing()	
 	
 	# Do imputation and return class
-	data_tools.dataClass.impute_data(data_info, adj_nan=adj_nan)	
+	data_info.impute_data(adj_nan=adj_nan)	
 	return(data_info)
 
