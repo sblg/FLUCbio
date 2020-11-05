02b93547e77c7725c9380d9d6168429f5af0a4af
ceciliabjensen <ceciliabjensen@gmail.com>
2020-11-05 09:59:38 +0100

    Update fluc_measure.py

--- fluc_measure.py
+++ fluc_measure.py
@@ -13,26 +13,8 @@ def fluc_measure(postprandial_data):
 	
 	# Check data input
 	data_info = data_tools.dataClass(postprandial_data)  
-	data_tools.dataClass.data_check(data_info, nan=False)  
+	data_info.data_check(nan=False)  
+	data_info.fluctuation()
 	
-	# Extract variable measures
-	pp_list = data_info.input[1]
-	
-	# Calculate measure of fluctuation
-	differences,differences_of_diff = [],[] 
-	
-	temp_list = np.zeros(len(pp_list))
-            
-	for i,value in enumerate(pp_list):
-	   if i != 0:
-		   temp_list[i-1] = abs(pp_list[i] - pp_list[i-1])
-		   differences.append(pp_list[i] - pp_list[i-1])
-	for i,value in enumerate(differences):
-	   if i != 0:
-		   differences_of_diff.append(abs(differences[i] - differences[i-1]))
-   
-	fluc_result = sum(differences_of_diff)
-	assert fluc_result >= 0, "Fluctuation should be positive"
-	
-	return(fluc_result)
+	return(data_info)
 
