0b51578081c202da9081425cdca67a700a44bbfe
ceciliabjensen <ceciliabjensen@gmail.com>
2020-11-05 10:16:29 +0100

    Update clust_sum.py

--- clust_sum.py
+++ clust_sum.py
@@ -5,40 +5,17 @@ Created on Thu Oct 15 12:53:30 2020
 @author: Cecilia Bang Jensen
 """
 
-import itertools
+import FLUCbio.data_functions as data_tools
 
 
-def clust_sum(image):
+def clust_sum(data_info):
 	"""  """
 	
 	
 	# give either data or image ??
 	
+
+	data_info.clustering()
 	
-	
-#	# Class object is created
-#	data_info = dc.dataClass(test)
-#	
-#	# Data check is performed 
-#	dc.dataClass.data_check(data_info,nan=False)
-#	
-#	# Create image
-#	dc.dataClass.image(data_info)
-#	
-	
-	### Clustering
-	all_sums = []
-	all_cluster = []
-	
-	all_sums.append(sum(image))
-		
-	count_dups = [sum(1 for _ in group) for _, group in itertools.groupby(image) if _ != 0]
-	sum_ = 0
-	for i in count_dups:
-		if i > 1:
-			sum_ += i
-	all_cluster.append(sum_)
-  
-	
-	return(all_cluster,all_sums)
+	return(data_info)
 
