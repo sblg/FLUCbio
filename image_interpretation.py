c28a4e0abff02152b1c123de3457c1086a05c2d9
ceciliabjensen <ceciliabjensen@gmail.com>
2020-11-17 08:56:52 +0100

    Update image_interpretation.py

--- image_interpretation.py
+++ image_interpretation.py
@@ -12,21 +12,14 @@ import numpy as np
 
 def image_interpretation(postprandial_data, num_interp_pts=100, grid_size=10, interpolation_type='linear', lower_bound=None, upper_bound=None):
 	
-	# do checks and tell to impute if necessary
-	
-
-
-
-	
 	# Class object is created
 	data_info = data_tools.dataClass(postprandial_data, num_interp_pts, grid_size, interpolation_type, lower_bound, upper_bound)
 	
 	# Data check is performed 
-	data_tools.dataClass.data_check(data_info,nan=False)
+	data_info.data_check(nan=False)
 	
 	# Create image
-	data_tools.dataClass.image(data_info)
-	
+	data_info.image()
 	
 	return(data_info)
 	
