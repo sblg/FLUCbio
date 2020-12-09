3af851dfaeeb4b581f7529b8e803dc631a0f2456
ceciliabjensen <ceciliabjensen@gmail.com>
2020-12-09 08:52:07 +0100

    Update image_interpretation.py

--- image_interpretation.py
+++ image_interpretation.py
@@ -10,14 +10,16 @@ import pandas as pd
 import numpy as np
 
 
-def image_interpretation(postprandial_data, num_interp_pts=100, grid_size=10, interpolation_type='linear', lower_bound=None, upper_bound=None):
+def image_interpretation(postprandial_data, num_interp_pts=100, grid_size=10, interpolation_type='linear', lower_bound=None, upper_bound=None, ignore_nan=False):
 	
 	# Class object is created
 	data_info = data_tools.dataClass(postprandial_data, num_interp_pts, grid_size, interpolation_type, lower_bound, upper_bound)
 	
 	# Data check is performed 
-	data_info.data_check(nan=False)
-	
+	if ignore_nan is True:
+		data_info.data_check(nan=True)
+	if ignore_nan is False:
+		data_info.data_check(nan=False)
 	# Create image
 	data_info.image()
 	
