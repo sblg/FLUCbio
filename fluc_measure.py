65a2b2392429c11294e070081d8a69396a36dd1e
ceciliabjensen <ceciliabjensen@gmail.com>
2020-12-09 08:50:31 +0100

    Update fluc_measure.py

--- fluc_measure.py
+++ fluc_measure.py
@@ -8,12 +8,15 @@ Created on Thu Oct 15 12:53:14 2020
 import numpy as np
 import FLUCbio.data_functions as data_tools
 
-def fluc_measure(postprandial_data):
+def fluc_measure(postprandial_data, ignore_nan=False):
 	""" Calculates a measure of fluctuation expecting evenly distributed data """
 	
 	# Check data input
 	data_info = data_tools.dataClass(postprandial_data)  
-	data_info.data_check(nan=False)  
+	if ignore_nan is True:
+		data_info.data_check(nan=True)
+	if ignore_nan is False:
+		data_info.data_check(nan=False)  
 	data_info.fluctuation()
 	
 	return(data_info)
